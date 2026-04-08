20f4ad721ef117bbe4ec95fff11b5ed8c625bf6a
Дмитрий <dimakol2005@mail.ru>
2026-04-08 16:14:58 +0500

    feat: util for counting tokens before request to the LLM

---
+++ src/memory/token_counter.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from langchain_core.messages import BaseMessage
+
+from app.services.llm_client import GigaChatClient
+
+
+class TokenCounter:
+
+    def __init__(self, llm: GigaChatClient):
+        self.llm = llm
+
+    def count_messages_tokens(
+        self,
+        messages: list[BaseMessage],
+    ) -> int:
+
+        return self.llm.client.get_num_tokens_from_messages(
+            messages
+        )
